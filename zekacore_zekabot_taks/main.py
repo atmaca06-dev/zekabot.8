140d7ae3606eeae351a18f89cc47fbed54051601
atmaca06-dev <s.ali.atmaca@outlook.com>
2025-05-13 01:33:10 +0300

    Delete zekacore_zekabot_taks/main.py

--- zekacore_zekabot_taks/main.py
+++
@@ -1,5 +0,0 @@
-from zekacore_zekabot_tasks.run_code import run_all_tasks
-
-if __name__ == "__main__":
-    print("Zekabot Render üzerinde başlatıldı, görevler yürütülüyor...")
-    run_all_tasks()
