db0f7e10ef982d9564546320e7f5c8ef40d7a53b
atmaca06-dev <s.ali.atmaca@outlook.com>
2025-05-13 02:51:32 +0300

    Update api_connect.py

--- api_connect.py
+++ api_connect.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
 import os
-import openai  # OpenAI modülünü yüklediysen
+import openai
 
 # .env dosyasını yükle
 load_dotenv()
 
 # .env içinden API anahtarını al
-api_key = os.getenv("API_KEY")
+api_key = os.environ.get("API_KEY")
 
 # OpenAI API'ye anahtarı tanımla
 openai.api_key = api_key
